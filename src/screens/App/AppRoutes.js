45f8518fbff292974668b14c330230f9b634f3c8
ChristosTeneketzoglou <christaras334@gmail.com>
2020-01-25 15:31:32 +0100

    feat:addCreatSurvey

--- src/screens/App/AppRoutes.js
+++ src/screens/App/AppRoutes.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
 import { Layout } from 'antd';
+import CreateSurvey from '../CreateSurvey';
 
 const { Content } = Layout;
 
-const AppContent = ({ text }) => (
+const AppContent = ({ children }) => (
   <Content
     style={{
       margin: '24px 16px',
@@ -13,20 +14,23 @@ const AppContent = ({ text }) => (
       minHeight: 280,
     }}
   >
-    {text}
+    {children}
   </Content>
 );
 
-const AppRoutes = () => (
-  <React.Fragment>
-    <Route exact path="/">
-      <AppContent text="Survey List" />
-    </Route>
+const AppRoutes = () => {
+  console.log(React);
+  return (
+    <React.Fragment>
+      <Route exact path="/">
+        <AppContent children={<CreateSurvey />} />
+      </Route>
 
-    <Route path="/create-survey">
-      <AppContent text="create survey" />
-    </Route>
-  </React.Fragment>
-);
+      <Route path="/create-survey">
+        <AppContent children="create survey" />
+      </Route>
+    </React.Fragment>
+  );
+};
 
 export default AppRoutes;
---
+++ src/screens/CreateSurvey/index.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class CreateSurvey extends Component {
+  render() {
+    return <div>create survey</div>;
+  }
+}
+
+export default CreateSurvey;
