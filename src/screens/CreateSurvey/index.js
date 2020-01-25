13ce1b5818f7ddf48655521f3972485175ba8b53
ChristosTeneketzoglou <christaras334@gmail.com>
2020-01-25 16:08:02 +0100

    feat:addtabs

--- src/screens/CreateSurvey/index.js
+++ src/screens/CreateSurvey/index.js
@@ -1,8 +1,46 @@
 import React, { Component } from "react";
+import { Tabs, Input } from "antd";
+import { Button } from "antd";
+
+const { TabPane } = Tabs;
 
 class CreateSurvey extends Component {
+  state = {
+    showAddQuestion: false,
+    saveQuestion: ""
+  };
+
+  handleShowQuestion4444 = () => {
+    this.setState({ showAddQuestion: true });
+  };
+
+  handleQuestion = e => {
+    console.log(e);
+    this.setState({ saveQuestion: e.target.value });
+  };
+
   render() {
-    return <div>create survey</div>;
+    const { showAddQuestion, saveQuestion } = this.state;
+
+    console.log(showAddQuestion, saveQuestion);
+    return (
+      <div>
+        <Tabs defaultActiveKey="1">
+          <TabPane tab="Create Survey" key="1">
+            {showAddQuestion ? (
+              <Input onChange={this.handleQuestion} />
+            ) : (
+              <Button onClick={this.handleShowQuestion4444}>
+                add question
+              </Button>
+            )}
+          </TabPane>
+          <TabPane tab="Preview Survey" key="2">
+            {saveQuestion}
+          </TabPane>
+        </Tabs>
+      </div>
+    );
   }
 }
 
