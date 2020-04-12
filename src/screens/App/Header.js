a7024466efb798c468148c4fac048dc01f7209dc
ChristosTeneketzoglou <christaras334@gmail.com>
2020-04-12 12:36:32 +0200

    refactor: style

--- src/screens/App/Header.js
+++ src/screens/App/Header.js
@@ -1,14 +1,15 @@
 import React from 'react';
-import { Layout, Icon } from 'antd';
+import { Icon } from 'antd';
+import { StyledHeader } from './styles';
 
 const Header = ({ collapsed, onIconClick }) => (
-  <Layout.Header style={{ background: '#fff', padding: 0 }}>
+  <StyledHeader>
     <Icon
       className="trigger"
       type={collapsed ? 'menu-unfold' : 'menu-fold'}
       onClick={onIconClick}
     />
-  </Layout.Header>
+  </StyledHeader>
 );
 
 export default Header;
--- src/screens/App/Shell.js
+++ src/screens/App/Shell.js
@@ -1,31 +1,9 @@
 import React from 'react';
 import { Layout } from 'antd';
-import styled from 'styled-components';
-
 import Sidebar from './Sidebar';
 import Routes from './Routes';
 import Header from './Header';
-
-const StyledLayout = styled(Layout)`
-  height: 100vh;
-  .trigger {
-    font-size: 18px;
-    line-height: 64px;
-    padding: 0 24px;
-    cursor: pointer;
-    transition: color 0.3s;
-    &:hover {
-      color: #1890ff;
-    }
-  }
-
-  .logo {
-    height: 32px;
-    background: rgba(255, 255, 255, 0.2);
-    margin: 16px;
-  }
-`;
-
+import { StyledLayout } from './styles';
 class Shell extends React.Component {
   state = {
     collapsed: false,
