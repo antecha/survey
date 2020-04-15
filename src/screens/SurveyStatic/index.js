1671216ccd1f28781afe0fef5c6879c425013eac
ChristosTeneketzoglou <christaras334@gmail.com>
2020-04-15 19:54:58 +0200

    add handlers events

--- src/screens/SurveyStatic/index.js
+++ src/screens/SurveyStatic/index.js
@@ -10,27 +10,49 @@ const RadioGroup = Radio.Group;
 const { Option } = Select;
 
 class SurveyStatic extends Component<{}, SurveyStaticType> {
+  handleChangeRadio = e => {
+    console.log('Changed Radio', e);
+  };
+
+  handleChangeInput = e => {
+    console.log('Changed Input', e);
+  };
+
+  handleChangeSlider = value => {
+    console.log('Changed Slider', value);
+  };
+
+  handleChangeDate = value => {
+    console.log('Changed Date', value);
+  };
+
+  handleChangeSwitch = (checked, event) => {
+    console.log('Switched', checked, event);
+  };
+  handleChangeSelect = (value, option) => {
+    console.log('Changed Selected item', value, option);
+  };
   render(): Element<'div'> {
     return (
       <div>
         <StyledQuestion>Q1. How do you ask in German 'what is your name' ?</StyledQuestion>
-        <RadioGroup>
+        <RadioGroup onChange={this.handleChangeRadio}>
           <StyledRadio value={1}>Wie geht es dir?</StyledRadio>
           <StyledRadio value={2}>Wie spaet es ist?</StyledRadio>
-          <StyledRadio value={3}>Wie heisst du?</StyledRadio>
+          <StyledRadio value={'Wie heisst du?'}>Wie heisst du?</StyledRadio>
         </RadioGroup>
 
         <StyledQuestion>Q2. What is your name?</StyledQuestion>
-        <Input placeholder="Enter your name" />
+        <Input onChange={this.handleChangeInput} placeholder="Enter your name" />
 
         <StyledQuestion>Q3. How many states are in Germany?</StyledQuestion>
-        <Slider range defaultValue={[5, 35]} />
+        <Slider onChange={this.handleChangeSlider} range defaultValue={[5, 35]} />
 
         <StyledQuestion>Q4. When is your birthday?</StyledQuestion>
-        <DatePicker />
+        <DatePicker onChange={this.handleChangeDate} />
 
         <StyledQuestion>Q5. What kind of coffee do you like?</StyledQuestion>
-        <Select defaultValue="freddo">
+        <Select onChange={this.handleChangeSelect} defaultValue="freddo">
           <Option value="freddo">freddo</Option>
           <Option value="frape">frape</Option>
           <Option value="capuccino">capuccino</Option>
@@ -40,7 +62,12 @@ class SurveyStatic extends Component<{}, SurveyStaticType> {
         </Select>
 
         <StyledQuestion>Q6. What is your gender?</StyledQuestion>
-        <Switch checkedChildren="Male" unCheckedChildren="Female" defaultChecked />
+        <Switch
+          onChange={this.handleChangeSwitch}
+          checkedChildren="Male"
+          unCheckedChildren="Female"
+          defaultChecked
+        />
 
         <StyledQuestion>
           <Button type="primary">Submit</Button>
